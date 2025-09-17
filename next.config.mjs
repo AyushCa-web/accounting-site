4d7b6b318e9aa72859d8953c886bd873a7cfce9d
AyushCa-web <caaayushmaheshwari@gmail.com>
2025-09-17 13:56:15 +0530

    removed base path

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/accounting-site' : '',
-  assetPrefix: isProd ? '/accounting-site/' : '',
+//   basePath: isProd ? '/accounting-site' : '',
+//   assetPrefix: isProd ? '/accounting-site/' : '',
   reactStrictMode: true,
   images: {
     unoptimized: true,
